233468c9cb7f276633b0ba7b66b244ff4b000e18
impaler <user123@server.fake>
2013-09-08 11:17:12 +1000

    improve color utils, definitions from haxeflixel thanks @Gama11

--- omni/utils/ColorUtils.hx
+++ omni/utils/ColorUtils.hx
@@ -4,59 +4,288 @@
  * ColorUtils
  * Useful color utils
  *
+ * Some definitions also from HaxeFlixel
+ *
  */
 class ColorUtils
 {
+	/**
+	 * 0xffff0012
+	 */
+	static public inline var RED:Int = 0xffff0000;
 
-    /**
- *  Return a random Color
- *
- * @return
- */
-    public static inline function randomColor():Int
-    {
-        return Std.int(Math.random() * WHITE);
-    }
+	/**
+	 * 0xffffff00
+	 */
+	static public inline var YELLOW:Int = 0xffffff00;
 
-    public static var WHITE:Int = 0xffffff;
+	/**
+	 * 0xff00f225
+	 */
+	static public inline var GREEN:Int = 0xff008000;
 
+	/**
+	 * 0xff0090e9
+	 */
+	static public inline var BLUE:Int = 0xff0000ff;
+
+	/**
+	 * 0xfff01eff
+	 */
+	static public inline var PINK:Int = 0xffffc0cb;
+
+	/**
+	 * 0xff800080
+	 */
+	static public inline var PURPLE:Int = 0xff800080;
+
+	/**
+	 * 0xffffffff
+	 */
+	static public inline var WHITE:Int = 0xffffffff;
+
+	/**
+	 * 0xff000000
+	 */
+	static public inline var BLACK:Int = 0xff000000;
+
+	/**
+	 * 0xff808080
+	 */
+	static public inline var GRAY:Int = 0xff808080;
+
+	/**
+	 * 0xff964B00
+	 */
+	static public inline var BROWN:Int = 0xff964B00;
+
+	/**
+	 * 0x00000000
+	 */
+	static public inline var TRANSPARENT:Int = 0x00000000;
+
+	/**
+     * Ivory is an off-white color that resembles ivory. 0xfffffff0
+     */
+	static public inline var IVORY:Int = 0xfffffff0;
+
+	/**
+     * Beige is a very pale brown. 0xfff5f5dc
+     */
+	static public inline var BEIGE:Int = 0xfff5f5dc;
+
+	/**
+     * Wheat is a color that resembles wheat. 0xfff5deb3
+     */
+	static public inline var WHEAT:Int = 0xfff5deb3;
+
+	/**
+     * Tan is a pale tone of brown. 0xffd2b48c
+     */
+	static public inline var TAN:Int = 0xffd2b48c;
+
+	/**
+     * Khaki is a light shade of yellow-brown similar to tan or beige. 0xffc3b091
+     */
+	static public inline var KHAKI:Int = 0xffc3b091;
+
+	/**
+     * Silver is a metallic color tone resembling gray that is a representation of the color of polished silver. 0xffc0c0c0
+     */
+	static public inline var SILVER:Int = 0xffc0c0c0;
+
+	/**
+     * Charcoal is a representation of the dark gray color of burned wood. 0xff464646
+     */
+	static public inline var CHARCOAL:Int = 0xff464646;
+
+	/**
+     * Navy blue is a dark shade of the color blue. 0xff000080
+     */
+	static public inline var NAVY_BLUE:Int = 0xff000080;
+
+	/**
+     * Royal blue is a dark shade of the color blue. 0xff084c9e
+     */
+	static public inline var ROYAL_BLUE:Int = 0xff084c9e;
+
+	/**
+     * A medium blue tone. 0xff0000cd
+     */
+	static public inline var MEDIUM_BLUE:Int = 0xff0000cd;
+
+	/**
+     * Azure is a color that is commonly compared to the color of the sky on a clear summer's day. 0xff007fff
+     */
+	static public inline var AZURE:Int = 0xff007fff;
+
+	/**
+     * Cyan is a color between blue and green. 0xff00ffff
+     */
+	static public inline var CYAN:Int = 0xff00ffff;
+
+	/**
+     * Aquamarine is a color that is a bluish tint of cerulean toned toward cyan. 0xff7fffd4
+     */
+	static public inline var AQUAMARINE:Int = 0xff7fffd4;
+
+	/**
+     * Teal is a low-saturated color, a bluish-green to dark medium. 0xff008080
+     */
+	static public inline var TEAL:Int = 0xff008080;
+
+	/**
+     * Forest green is a green color resembling trees and other plants in a forest. 0xff228b22
+     */
+	static public inline var FOREST_GREEN:Int = 0xff228b22;
+
+	/**
+     * Olive is a dark yellowish green or greyish-green color like that of unripe or green olives. 0xff808000
+     */
+	static public inline var OLIVE:Int = 0xff808000;
+
+	/**
+     * Chartreuse is a color halfway between yellow and green. 0xff7fff00
+     */
+	static public inline var CHARTREUSE:Int = 0xff7fff00;
+
+	/**
+     * Lime is a color three-quarters of the way between yellow and green. 0xffbfff00
+     */
+	static public inline var LIME:Int = 0xffbfff00;
+
+	/**
+     * Golden is one of a variety of yellow-brown color blends used to give the impression of the color of the element gold. 0xffffd700
+     */
+	static public inline var GOLDEN:Int = 0xffffd700;
+
+	/**
+     * Goldenrod is a color that resembles the goldenrod plant. 0xffdaa520
+     */
+	static public inline var GOLDENROD:Int = 0xffdaa520;
+
+	/**
+     * Coral is a pinkish-orange color. 0xffff7f50
+     */
+	static public inline var CORAL:Int = 0xffff7f50;
+
+	/**
+     * Salmon is a pale pinkish-orange to light pink color, named after the color of salmon flesh. 0xfffa8072
+     */
+	static public inline var SALMON:Int = 0xfffa8072;
+
+	/**
+     * Hot Pink is a more saturated version of the color pink. 0xfffc0fc0
+     */
+	static public inline var HOT_PINK:Int = 0xfffc0fc0;
+
+	/**
+     * Fuchsia is a vivid reddish or pink color named after the flower of the fuchsia plant. 0xffff77ff
+     */
+	static public inline var FUCHSIA:Int = 0xffff77ff;
+
+	/**
+     * Puce is a brownish-purple color. 0xffcc8899
+     */
+	static public inline var PUCE:Int = 0xffcc8899;
+
+	/**
+     * Mauve is a pale lavender-lilac color. 0xffe0b0ff
+     */
+	static public inline var MAUVE:Int = 0xffe0b0ff;
+
+	/**
+     * Lavender is a pale tint of violet. 0xffb57edc
+     */
+	static public inline var LAVENDER:Int = 0xffb57edc;
+
+	/**
+     * Plum is a deep purple color. 0xff843179
+     */
+	static public inline var PLUM:Int = 0xff843179;
+
+	/**
+     * Indigo is a deep and bright shade of blue. 0xff4b0082
+     */
+	static public inline var INDIGO:Int = 0xff4b0082;
+
+	/**
+     * Maroon is a dark brownish-red color. 0xff800000
+     */
+	static public inline var MAROON:Int = 0xff800000;
+
+	/**
+     * Crimson is a strong, bright, deep red color. 0xffdc143c
+     */
+	static public inline var CRIMSON:Int = 0xffdc143c;
+
+	/**
+     * 0xffc0c0c0
+     */
     public static var LIGHT_GRAY:Int = 0xc0c0c0;
 
+	/**
+     * 0xff808080
+     */
     public static var GRAY:Int = 0x808080;
 
-    public static var DARK_GRAY:Int = 0x404040;
+	/**
+     * 0xff404040
+     */
+    public static var DARK_GRAY:Int = 0xff404040;
 
-    public static var BLACK:Int = 0x000000;
+	/**
+     * 0xffca2900
+     */
+    public static var DARKER_ORANGE:Int = 0xffca2900;
 
-    public static var RED:Int = 0xff0000;
+	/**
+     * 0xfffe963e
+     */
+    public static var ORANGE_BRIGHT:Int = 0xfffe963e;
 
-    public static var PINK:Int = 0xffafaf;
+	/**
+     * 0xffff6c00
+     */
+    public static var ORANGE:Int = 0xffff6c00;
 
-    public static var DARKER_ORANGE:Int = 0xca2900;
+	/**
+     * 0xffffa360
+     */
+    public static var LIGHT_ORANGE:Int = 0xffffa360;
 
-    public static var ORANGE_BRIGHT:Int = 0xfe963e;
+	/**
+     * 0xffFFC200
+     */
+    public static var HALO_ORANGE:Int = 0xffFFC200;
 
-    public static var ORANGE:Int = 0xff6c00;
+	/**
+     * 0xffd4ff00
+     */
+    public static var GREEN_LIGHT:Int = 0xffd4ff00;
 
-    public static var LIGHT_ORANGE:Int = 0xffa360;
+	/**
+     * 0xff80FF4D
+     */
+    public static var HALO_GREEN:Int = 0xff80FF4D;
 
-    public static var HALO_ORANGE:Int = 0xFFC200;
+	/**
+     * 0xffff00ff
+     */
+    public static var MAGENTA:Int = 0xffff00ff;
 
-    public static var YELLOW:Int = 0xffff00;
+	/**
+     * 0xff2BF5F5
+     */
+    public static var HALO_BLUE:Int = 0xff2BF5F5;
 
-    public static var GREEN:Int = 0x00ff00;
-
-    public static var GREEN_LIGHT:Int = 0xd4ff00;
-
-    public static var HALO_GREEN:Int = 0x80FF4D;
-
-    public static var MAGENTA:Int = 0xff00ff;
-
-    public static var CYAN:Int = 0x00ffff;
-
-    public static var BLUE:Int = 0x0000ff;
-
-    public static var HALO_BLUE:Int = 0x2BF5F5;
+	/**
+	*  Simple mehtod to return a random Color
+	*/
+	public static inline function randomColor():Int
+	{
+		return Std.int(Math.random() * WHITE);
+	}
 
     public static function getRGBWith(r:Int, g:Int, n:Int):Int
     {
